cd6a35877f2ccd4e11d616bd0532e0a49f073479
Qays H. Poonawala <qayshp@gmail.com>
2022-08-08 11:42:52 -0700

    Fix versioned url to include patch number

--- Casks/vimmotion.rb
+++ Casks/vimmotion.rb
@@ -5,8 +5,8 @@ cask 'vimmotion' do
 
   version '1.2.3'
   sha256 'c14bb9355b5191d15a5c7224c48763bf167ca926af39206d308b1cf9aca00c00'
-
-  url "https://github.com/dwarvesf/VimMotionApp/releases/download/v#{version}/VimMotion.#{version.major_minor}.dmg"
+https://github.com/dwarvesf/VimMotionApp/releases/download/v1.2.3/VimMotion.1.2.3.dmg
+  url "https://github.com/dwarvesf/VimMotionApp/releases/download/v#{version}/VimMotion.#{version.major_minor_patch}.dmg"
   name 'VimMotion'
   homepage 'https://github.com/dwarvesf/VimMotionApp/'
 
